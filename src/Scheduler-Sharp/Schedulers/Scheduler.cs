c169fb20ee481cd72f971a2f824932946a8fbfcd
Wilgnne <w.khawanbarbosa@gmail.com>
2019-05-01 19:37:00 -0300

    Structured Process Classes
    Classes de processos estruturadas

--- src/Scheduler-Sharp/Schedulers/Scheduler.cs
+++ src/Scheduler-Sharp/Schedulers/Scheduler.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using SchedulerSharp.Models;
 
 namespace SchedulerSharp.Schedulers
 {
     public class Scheduler
     {
-        public List<LogProcess> Finish { get; protected set; }
-        protected List<Process> forExec;
+        public List<PlotableProcess> Finish { get; protected set; }
+        protected List<EscalonableProcess> forExec;
 
         protected int clkTime = 0;
 
         public virtual void ProcessData() { }
 
         /*Executar Processo*/
-        protected void RunProcess (Process process)
+        protected void RunProcess (EscalonableProcess process)
         {
             /*Atualiza o tempo de execução do processo*/
             process.Run();
             /*Adiciona uma referencia do mesmo a lista de finalizados*/
-            Finish.Add(new LogProcess (process, clkTime));
+            Finish.Add(new PlotableProcess (process, clkTime));
             /*Se o processo tiver terminado sua execução por completo o remove
             da lista de processos para execução*/
             if (process.Runtime == 0)
