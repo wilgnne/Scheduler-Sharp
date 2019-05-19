539546889ac82c746e98785d36f23fe7530ae09c
GabiFaria <gabrielafariaoliveira@gmail.com>
2019-05-18 22:59:56 -0300

    FCFS
    Algoritmo de escalonador FCFS.

--- src/Scheduler-Sharp/Schedulers/FCFS.cs
+++ src/Scheduler-Sharp/Schedulers/FCFS.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using SchedulerSharp.Models;
 namespace SchedulerSharp.Schedulers
 {
-    public class FCFS : Scheduler
+    public static class FCFS
     {
-        public FCFS()
+        public static List<Process> InsertionSort_Processes(List<Process> list)
         {
+            int i, n, flag, j;
+            n = list.Count;
+            for (i = 1; i < n; i++)
+            {
+                Process value = list[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (value.ArrivalTime < list[j].ArrivalTime)
+                    {
+                        list[j + 1] = list[j];
+                        j--;
+                        list[j + 1] = value;
+                    }
+                    else flag = 1;
+                }
+            }
+            return list;
         }
-
-        public override void ProcessData ()
+        public static List<PlotableProcess> Schedulering(List<Process> list)
         {
+            int execTime = 0;
+            List<PlotableProcess> listPlotable = new List<PlotableProcess>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                EscalonableProcess escalonador = new EscalonableProcess(list[i]);
+                while (escalonador.Run())
+                {
+                    listPlotable.Add(new PlotableProcess(escalonador, execTime));
+                    execTime++;
+                }
+            }
+
+            return listPlotable;
         }
     }
 }
