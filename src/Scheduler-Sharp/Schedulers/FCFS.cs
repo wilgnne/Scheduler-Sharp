672d59497169b768782c1f7a1bca59f068502842
GabiFaria <gabrielafariaoliveira@gmail.com>
2019-05-22 13:42:27 -0300

    Insertion Sort
    Adicionei o Insertion Sort na pasta Scheduler

--- src/Scheduler-Sharp/Schedulers/FCFS.cs
+++ src/Scheduler-Sharp/Schedulers/FCFS.cs
@@ -5,27 +5,6 @@ namespace SchedulerSharp.Schedulers
 {
     public static class FCFS
     {
-        public static List<Process> InsertionSort_Processes(List<Process> list)
-        {
-            int i, n, flag, j;
-            n = list.Count;
-            for (i = 1; i < n; i++)
-            {
-                Process value = list[i];
-                flag = 0;
-                for (j = i - 1; j >= 0 && flag != 1;)
-                {
-                    if (value.ArrivalTime < list[j].ArrivalTime)
-                    {
-                        list[j + 1] = list[j];
-                        j--;
-                        list[j + 1] = value;
-                    }
-                    else flag = 1;
-                }
-            }
-            return list;
-        }
         public static List<PlotableProcess> Schedulering(List<Process> list)
         {
             int execTime = 0;
---
+++ src/Scheduler-Sharp/Schedulers/InsetionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using SchedulerSharp.Models;
+namespace SchedulerSharp.Schedulers
+{
+    public static class InsertionSort
+    {
+        public static List<Process> InsertionSort_Processes(List<Process> list)
+        {
+            int i, n, flag, j;
+            n = list.Count;
+            for (i = 1; i < n; i++)
+            {
+                Process value = list[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (value.ArrivalTime < list[j].ArrivalTime)
+                    {
+                        list[j + 1] = list[j];
+                        j--;
+                        list[j + 1] = value;
+                    }
+                    else flag = 1;
+                }
+            }
+            return list;
+        }
+    }
+}
